8a3bdc0accf8f0302387c4638be60177158b19f8
v-Sharan <vsharansago@gmail.com>
2023-06-05 23:50:39 +0530

    erro handling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import CustomInput from "./components/CustomInput";
-
 import axios from "axios";
 
 function App() {
@@ -24,6 +23,8 @@ function App() {
     address: "",
   });
 
+  const [error, setError] = useState();
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -31,11 +32,12 @@ function App() {
     setLoading(true);
 
     axios
-      .post("http://localhost:8000/form", form)
+      .post("https://hack-form.onrender.com/form", form)
       .then((res) => {
         console.log(res);
       })
       .catch((err) => {
+        setError(err.response.data.message);
         console.log(err);
       })
       .finally(() => setLoading(false));
@@ -47,6 +49,24 @@ function App() {
   return (
     <main className="flex h-[130vh] w-[100%] min-h-screen bg-center bg-no-repeat bg-[url('https://res.cloudinary.com/dfje97i0k/image/upload/c_scale,h_1200,w_1500/v1685986831/background_i6229o.jpg')] bg-gray-700 bg-blend-multiply">
       <div className="w-[50%] mx-auto my-auto bg-white border border-gray-200 rounded-lg shadow sm:p-2 md:p-4 dark:bg-gray-800 dark:border-gray-700">
+        {!!error && (
+          <div className="alert alert-error">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>{error}</span>
+          </div>
+        )}
         <h5 className="text-xl font-medium text-gray-900 dark:text-white text-center">
           Hostel Outing Form
         </h5>
--- Frontend/src/components/CustomInput.jsx
+++ Frontend/src/components/CustomInput.jsx
@@ -16,7 +16,7 @@ const CustomInput = ({ value, onChage, label, type, name, placeholder }) => {
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
         placeholder={placeholder}
         onChange={onChage}
-        required
+        // required
         value={value}
       />
     </div>
